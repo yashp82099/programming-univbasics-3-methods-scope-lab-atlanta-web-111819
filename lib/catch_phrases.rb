a9d2c763bc2e854200b48bd96c0a618fac1990ca
yash patel <yash82099@yahoo.com>
2019-10-10 19:26:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
 
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 
 
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   return status
 end
 
